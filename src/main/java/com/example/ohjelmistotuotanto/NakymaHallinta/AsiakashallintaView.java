b72b8f43ddc175d9fe7e9c48067648101860129c
BuildTools <Botushka@users.noreply.github.com>
2023-04-06 18:01:49 +0300

    1-4 arvionnin näkymien luokat

---
+++ src/main/java/com/example/ohjelmistotuotanto/NakymaHallinta/AsiakashallintaView.java
@@ -0,0 +1,38 @@
+package com.example.ohjelmistotuotanto.NakymaHallinta;
+
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+
+public class AsiakashallintaView extends VBox {
+    public AsiakashallintaView() {
+        Button lisaaAsiakas = new Button("Lisää asiakas");
+        lisaaAsiakas.setOnAction(e -> lisaaAsiakas());
+
+        Button poistaAsiakas = new Button("Poista asiakas");
+        poistaAsiakas.setOnAction(e -> poistaAsiakas());
+
+        Button muokkaaAsiakas = new Button("Muokkaa asiakas");
+        muokkaaAsiakas.setOnAction(e -> muokkaaAsiakas());
+
+        Button haeAsiakas = new Button("Hae asiakas");
+        haeAsiakas.setOnAction(e -> haeAsiakas());
+
+        getChildren().addAll(lisaaAsiakas, poistaAsiakas, muokkaaAsiakas, haeAsiakas);
+    }
+
+    private void lisaaAsiakas() {
+        // Toteuta asiakkaan lisääminen
+    }
+
+    private void poistaAsiakas() {
+        // Toteuta asiakkaan poistaminen
+    }
+
+    private void muokkaaAsiakas() {
+        // Toteuta asiakkaan muokkaaminen
+    }
+
+    private void haeAsiakas() {
+        // Toteuta asiakkaan hakeminen
+    }
+}
---
+++ src/main/java/com/example/ohjelmistotuotanto/NakymaHallinta/MajoitusvarausHallintaView.java
@@ -0,0 +1,38 @@
+package com.example.ohjelmistotuotanto.NakymaHallinta;
+
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+
+public class MajoitusvarausHallintaView extends VBox {
+    public MajoitusvarausHallintaView() {
+        Button lisaaVaraus = new Button("Lisää varaus");
+        lisaaVaraus.setOnAction(e -> lisaaVaraus());
+
+        Button poistaVaraus = new Button("Poista varaus");
+        poistaVaraus.setOnAction(e -> poistaVaraus());
+
+        Button muokkaaVaraus = new Button("Muokkaa varaus");
+        muokkaaVaraus.setOnAction(e -> muokkaaVaraus());
+
+        Button haeVaraus = new Button("Hae varaus");
+        haeVaraus.setOnAction(e -> haeVaraus());
+
+        getChildren().addAll(lisaaVaraus, poistaVaraus, muokkaaVaraus, haeVaraus);
+    }
+
+    private void lisaaVaraus() {
+        // Toteuta varauksen lisääminen
+    }
+
+    private void poistaVaraus() {
+        // Toteuta varauksen poistaminen
+    }
+
+    private void muokkaaVaraus() {
+        // Toteuta varauksen muokkaaminen
+    }
+
+    private void haeVaraus() {
+        // Toteuta varauksen hakeminen
+    }
+}
